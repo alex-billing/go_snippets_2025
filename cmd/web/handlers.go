e15691f947979ca8add508f6085518d5500e872f
Alexander Yablokov <tajikistan@rambler.ru>
2025-09-05 16:17:44 +0500

    Execute root template

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -28,7 +28,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	// Use the Execute() method on the template set to write the template
 	// content as the response body.
-	err = ts.ExecuteTemplate(w, "layout", nil)
+	err = ts.Execute(w, nil)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
