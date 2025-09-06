527cfc64265f6ffd3b526b93058e2db1078e3f1a
Alexander Yablokov <tajikistan@rambler.ru>
2025-09-06 16:01:44 +0500

    middleware to prevent browsing static directory

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
+
+func neuter(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			if strings.HasSuffix(r.URL.Path, "/") {
+				http.NotFound(w, r)
+				return
+			}
+
+			next.ServeHTTP(w, r)
+		},
+	)
+}
 
 func main() {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
 
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
