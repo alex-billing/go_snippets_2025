21210629943dd0fc034ed1a2623542604a65dabf
Alexander Yablokov <tajikistan@rambler.ru>
2025-09-11 13:13:28 +0500

    Added logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,37 +1,65 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
-	"strings"
+	"os"
+	"path/filepath"
 )
 
-func neuter(next http.Handler) http.Handler {
-	return http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			if strings.HasSuffix(r.URL.Path, "/") {
-				http.NotFound(w, r)
-				return
+type neuteredFileSystem struct {
+	fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := f.Stat()
+	if err != nil {
+		return nil, err
+	}
+
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
 			}
 
-			next.ServeHTTP(w, r)
-		},
-	)
+			return nil, err
+		}
+	}
+
+	return f, nil
 }
 
 func main() {
+
+	addr := flag.String("addr", ":4003", "HTTP network address")
+	flag.Parse()
+
+	loggerHandler := slog.NewTextHandler(os.Stdout, nil)
+	logger := slog.New(loggerHandler)
+
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", neuter(fileServer)))
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static", http.NotFoundHandler())
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4003")
-	err := http.ListenAndServe(":4003", mux)
+	logger.Info("starting server", slog.Any("addr", *addr))
+	err := http.ListenAndServe(*addr, mux)
 
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
